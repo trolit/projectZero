e086e54363b06fe450e6e21c8db9d1acf337d8a3
Paweł Idzikowski <iddziku@gmail.com>
2019-06-21 12:26:36 +0200

    Rozwój projektu

---
+++ projectZero/Assets/Scripts/Game/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Game
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
