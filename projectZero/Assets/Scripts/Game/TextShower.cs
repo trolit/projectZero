344902d4aa33a8a65c61141d0f6a6ab09ce80c32
Paweł Idzikowski <iddziku@gmail.com>
2019-06-23 16:33:53 +0200

    Dodanie notyfikatu przy zapisaniu ustawien

---
+++ projectZero/Assets/Scripts/Game/TextShower.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Game
+{
+    public class TextShower : MonoBehaviour
+    {
+        [SerializeField]
+        private Text Text;
+
+        [SerializeField]
+        private string Description;
+
+        public void DisplayText()
+        {
+            if (string.IsNullOrEmpty(Description))
+            {
+                Debug.Log("Żaden tekst nie został wprowadzony do obiektu => " + Text);
+            }
+
+            Text.text = Description;
+
+            // run HideText function after 1.5 seconds
+            Invoke("HideText", 1.5f);
+        }
+
+        public void HideText()
+        {
+            Text.text = "";
+        }
+    }
+}
