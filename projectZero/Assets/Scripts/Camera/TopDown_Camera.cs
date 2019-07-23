9439796d7ebe83a44a2a0dd27139c3c61cae2aed
Paweł Idzikowski <iddziku@gmail.com>
2019-07-23 16:09:07 +0200

    Praca nad minigra

---
+++ projectZero/Assets/Scripts/Camera/TopDown_Camera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Camera
+{
+    public class TopDown_Camera : MonoBehaviour
+    {
+        public Transform Target;
+
+        public float Height = 10f;
+
+        public float Distance = 20f;
+
+        public float Angle = 45f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
