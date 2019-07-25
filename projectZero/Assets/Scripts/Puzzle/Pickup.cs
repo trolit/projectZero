5d61bba04b427cfc24ad835f74e11a6a37c0b01c
Paweł Idzikowski <iddziku@gmail.com>
2019-07-25 13:01:16 +0200

    Praca nad poziomem + walka z przenoszeniem obiektow

---
+++ projectZero/Assets/Scripts/Puzzle/Pickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Puzzle
+{
+    public class Pickup : MonoBehaviour
+    {
+        private GameObject _player;
+
+        void Start()
+        {
+            _player = GameObject.FindWithTag("Player");
+        }
+
+        void OnCollisionStay(Collision block)
+        {
+            if (block.gameObject.tag == "Draggable"
+                && Input.GetKey(KeyCode.F))
+            {
+                // block.collider.enabled = false;
+                block.transform.parent = _player.transform;
+            }
+            else
+            {
+                block.transform.parent = null;
+            }
+        }
+    }
+}
---
+++ projectZero/Assets/Scripts/Puzzle/Pickupable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class Pickupable : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
