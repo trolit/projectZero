b2f6f9460063213762736555f0bf191a29ddd3b2
Paweł Idzikowski <iddziku@gmail.com>
2019-07-30 14:53:08 +0200

    Dodanie menu pauzy podczas gry

---
+++ projectZero/Assets/Scripts/Game/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public static bool GameIsPaused = false;
+
+        public GameObject PauseMenuUi;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (GameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            PauseMenuUi.SetActive(false);
+
+            Time.timeScale = 1f;
+
+            GameIsPaused = false;
+        }
+
+        void Pause()
+        {
+            PauseMenuUi.SetActive(true);
+
+            Time.timeScale = 0f;
+
+            GameIsPaused = true;
+        }
+    }
+}
