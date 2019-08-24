ee3effc5490a662eda64adfe7403ace6135cea08
Paweł Idzikowski <iddziku@gmail.com>
2019-08-24 14:28:21 +0200

    Dodanie skryptu TransferMoney dla minigry puzzle

---
+++ projectZero/Assets/Scripts/Minigame_Puzzle/TransferMoney.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Minigame_Puzzle
+{
+    public class TransferMoney : MonoBehaviour
+    {
+        public void TransferMoneyOnPlayerAccount(int amount)
+        {
+            var currentMoney = PlayerPrefs.GetInt("money");
+
+            var newMoneyState = currentMoney + amount;
+
+            PlayerPrefs.SetInt("money",
+                newMoneyState);
+        }
+    }
+}
