dff64a7de5ec63b996dfa59e30619c39c76d7677
Paweł Idzikowski <iddziku@gmail.com>
2019-08-25 19:30:26 +0200

    Dodanie skryptu losowego przemieszczania sie na bazie NavMeshAgent

---
+++ projectZero/Assets/Scripts/Game/MoveRandomly.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.Game
+{
+    public class MoveRandomly : MonoBehaviour
+    {
+        public float Timer;
+
+        public int NewTarget;
+
+        public NavMeshAgent Nav;
+
+        public Vector3 Target;
+
+        public int MinX;
+
+        public int MaxX;
+
+        public int MinZ;
+
+        public int MaxZ;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Nav = gameObject.GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Timer += Time.deltaTime;
+
+            if (Timer >= NewTarget)
+            {
+                FindNewTarget();
+
+                Timer = 0;
+            }
+        }
+
+        void FindNewTarget()
+        {
+            float x = gameObject.transform.position.x;
+            float z = gameObject.transform.position.z;
+
+            float xPos = Random.Range(MinX, MaxX);
+            float zPos = Random.Range(MinZ, MaxZ);
+
+            Target = new Vector3(xPos, gameObject.transform.position.y, zPos);
+
+            Nav.SetDestination(Target);
+        }
+    }
+}
