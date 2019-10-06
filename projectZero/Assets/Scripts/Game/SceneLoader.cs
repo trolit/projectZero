04dd3bd6bcb1029fb097c56b2c52c26e3908d232
Paweł Idzikowski <iddziku@gmail.com>
2019-10-06 11:29:56 +0200

    Oznaczenenie metody jako "Obsolete"

--- projectZero/Assets/Scripts/Game/SceneLoader.cs
+++ projectZero/Assets/Scripts/Game/SceneLoader.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Game
 {
     public class SceneLoader : MonoBehaviour
     {
+        [Obsolete("Use LoadSceneAsync to prevent forcing all resources to load scene")]
         public void LoadScene(string sceneName)
         {
             SceneManager.LoadScene(sceneName);
