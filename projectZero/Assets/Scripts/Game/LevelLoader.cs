f0b2670c26dc7117b661f199d1e3f68b296e4dd1
Paweł Idzikowski <iddziku@gmail.com>
2019-10-06 11:59:08 +0200

    Dodanie skryptu LevelLoader

---
+++ projectZero/Assets/Scripts/Game/LevelLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Game
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider _slider;
+
+        [SerializeField]
+        private GameObject _loadingScreen;
+
+        [SerializeField]
+        private string _sceneName;
+
+        [SerializeField]
+        private Text _progressText;
+
+        public void LoadLevel()
+        {
+            StartCoroutine(LoadAsynchronously());
+        }
+
+        IEnumerator LoadAsynchronously()
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(_sceneName);
+
+            _loadingScreen.SetActive(true);
+
+            while (operation.isDone == false)
+            {
+                float progress = Mathf.Clamp01(operation.progress / .9f);
+
+                _slider.value = progress;
+
+                _progressText.text = progress * 100f + "%";
+
+                yield return null;
+            }
+        }
+    }
+}
