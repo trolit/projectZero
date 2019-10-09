83fa5bccc4a224e0246d3d157ccab397b980eb68
Paweł Idzikowski <iddziku@gmail.com>
2019-10-09 21:17:43 +0200

    Modyfikacja skryptu CommandIsReady

--- projectZero/Assets/Scripts/Dev/CommandIsReady.cs
+++ projectZero/Assets/Scripts/Dev/CommandIsReady.cs
@@ -1,24 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using static Console.DevConsole;
 
 namespace Console
 {
     public class CommandIsReady : ConsoleCommand
     {
-        public override string Name { get; protected set; }
-        public override string Command { get; protected set; }
-        public override string Description { get; protected set; }
-        public override string Help { get; protected set; }
-        public override string Example { get; protected set; }
+        public sealed override string Name { get; protected set; }
+        public sealed override string Command { get; protected set; }
+        public sealed override string Description { get; protected set; }
+        public sealed override string Help { get; protected set; }
+        public sealed override string Example { get; protected set; }
 
         public CommandIsReady()
         {
-            Name = "Help";
-            Command = "help";
-            Description = "Returns description for specified command (or all available commands if parameter is not specified)";
-            Help = "Syntax: help <command name> \n" +
-                   $"<color={OptionalColor}><command name></color> is optional";
-            Example = "help set";
+            Name = "Is Ready";
+            Command = "isReady";
+            Description = "Returns MODEL=TRUE (id) if character is picked, else MODEL=FALSE.";
+            Help = "Syntax: isReady";
+            Example = "isReady";
 
             AddCommandToConsole();
         }
@@ -27,37 +28,17 @@ namespace Console
         {
             if (data.Length == 1)
             {
-                AddStaticMessageToConsole("--------------------------------------------------");
-                AddStaticMessageToConsole("Available commands");
+                var modelId = PlayerPrefs.GetInt("model");
+                int[] correctValues = {1, 2, 3};
 
-                int counter = 1;
-
-                foreach (KeyValuePair<string, ConsoleCommand> command in Commands)
+                if (correctValues.Contains(modelId))
                 {
-                    AddStaticMessageToConsole(counter + ") " + command.Key);
-
-                    counter++;
+                    AddStaticMessageToConsole($"MODEL = <color={OptionalColor}>TRUE</color> ({modelId})");
                 }
-            }
-            else if (data.Length == 2 && Commands.ContainsKey(data[1].ToLower()))
-            {
-                var parameter = data[1].ToLower();
-
-                var command = Commands[parameter];
-
-                AddStaticMessageToConsole("--------------------------------------------------");
-                AddStaticMessageToConsole("<b>Title of command</b>");
-                AddStaticMessageToConsole(command.Name + "\n");
-                AddStaticMessageToConsole("<b>Description</b>");
-                AddStaticMessageToConsole(command.Description + "\n");
-                AddStaticMessageToConsole("<b>Usage</b>");
-                AddStaticMessageToConsole(command.Help + "\n");
-                AddStaticMessageToConsole("<b>Example</b>");
-                AddStaticMessageToConsole(command.Example + "\n");
-            }
-            else if (Commands.ContainsKey(data[1].ToLower()) == false)
-            {
-                AddStaticMessageToConsole(NotRecognized);
+                else
+                {
+                    AddStaticMessageToConsole($"MODEL = <color={RequiredColor}>FALSE</color>");
+                }
             }
             else
             {
