738ddee537ac4f1ad87b0c5142ef6a1df1178306
Paweł Idzikowski <iddziku@gmail.com>
2019-10-09 21:51:46 +0200

    Korekta VerifyMedal żeby w przypadku odblokowaniu medalu były poprawne strzałki :)

--- projectZero/Assets/Scripts/Game/VerifyMedal.cs
+++ projectZero/Assets/Scripts/Game/VerifyMedal.cs
@@ -60,7 +60,7 @@ namespace Assets.Scripts.Game
                 // Change arrow place texture only ON objects THAT 
                 // have _maxValue different than 5. 5 is restricted
                 // for main medal which doesnt have arrows specified
-                if (_maxValue == 5)
+                if (_maxValue != 5)
                 {
                     _arrowPlace.texture = _unlockedArrow;
                 }
@@ -69,6 +69,7 @@ namespace Assets.Scripts.Game
             }
 
             _info.SetActive(false);
+
             _amountText.text = "";
         }
 
@@ -77,7 +78,9 @@ namespace Assets.Scripts.Game
             if (_isUnlocked == false)
             {
                 _amountText.text = $"{_storedValue} / {_maxValue}";
+
                 _info.SetActive(true);
+
                 _panelRectTransform.SetAsLastSibling();
             }
         }
