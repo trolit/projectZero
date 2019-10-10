295d90a07e078d050eda9151e690525eef2abcc8
Paweł Idzikowski <iddziku@gmail.com>
2019-10-10 19:49:50 +0200

    Rozwój skryptów na potrzeby wczytywania minigier

--- projectZero/Assets/Scripts/Game/UpdateMinigameLoaderData.cs
+++ projectZero/Assets/Scripts/Game/UpdateMinigameLoaderData.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.Game
@@ -9,21 +10,74 @@ namespace Assets.Scripts.Game
         private Text _languageText;
 
         [SerializeField]
-        private Slider _slider;
+        private Image _sliderFillImage;
 
         [SerializeField]
         private Text _descriptionText;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [SerializeField]
+        private List<Texture> _imagesList;
 
-        // Update is called once per frame
-        void Update()
+        private Color _lime = new Color(0f, 255f, 0f);
+
+        private Color _orange = new Color(255f, 165f, 0f);
+
+        // Start is called before the first frame update
+        void Awake()
         {
-        
+            var baseText = "Wczytywanie poziomu krainy";
+
+            _languageText.text = baseText;
+
+            if (LevelLoader.CrossSceneInformation == null)
+            {
+                _languageText.text = "No language set.";
+
+                _sliderFillImage.color = Color.red;
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("JS"))
+            {
+                _languageText.text += "<color=cyan> JavaScript</color>";
+
+                _sliderFillImage.color = Color.cyan;
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("C#"))
+            {
+                _languageText.text += "<color=lime> C#</color>";
+
+                _sliderFillImage.color = _lime;
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("Java"))
+            {
+                _languageText.text += "<color=yellow> Java</color>";
+
+                _sliderFillImage.color = Color.yellow;
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("HTML"))
+            {
+                _languageText.text += "<color=magenta> HTML</color>";
+
+                _sliderFillImage.color = Color.magenta;
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("PHP"))
+            {
+                _languageText.text += "<color=orange> PHP</color>";
+
+                _sliderFillImage.color = _orange;
+            }
+
+            if (TextStorage.Texts != null)
+            {
+                var max = TextStorage.Texts.Count;
+
+                var value = Random.Range(0, max);
+
+                _descriptionText.text = TextStorage.Texts[value];
+            }
+            else
+            {
+                _descriptionText.text = "No description provided.";
+            }
         }
     }
 }
