7463fca6131c33bdf77ddf21170a728226cf6241
Paweł Idzikowski <iddziku@gmail.com>
2019-10-10 16:39:05 +0200

    Przygotowanie sceny do wyświetlenia PEGI oraz skryptu wspierającego

---
+++ projectZero/Assets/Scripts/Game/PegiInvoker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Game
+{
+    public class PegiInvoker : MonoBehaviour
+    {
+        [SerializeField]
+        private RawImage _source;
+
+        [SerializeField]
+        private string _followingSceneName;
+
+        IEnumerator Start()
+        {
+            _source.canvasRenderer.SetAlpha(0.0f);
+
+            FadeIn();
+
+            yield return new WaitForSeconds(2.5f);
+
+            FadeOut();
+
+            yield return new WaitForSeconds(2.5f);
+
+            SceneManager.LoadSceneAsync(_followingSceneName);
+        }
+
+        void FadeIn()
+        {
+            _source.CrossFadeAlpha(1.0f, 1.5f, false);
+        }
+
+        void FadeOut()
+        {
+            _source.CrossFadeAlpha(0.0f, 2.5f, false);
+        }
+    }
+}
