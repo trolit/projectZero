dafb9fbd34de67649fbcc6f764af6b70242e0943
Paweł Idzikowski <iddziku@gmail.com>
2019-10-10 19:32:16 +0200

    Praca nad skryptem aktualizującym dane znajdujące się na ekranie wczytywania

---
+++ projectZero/Assets/Scripts/Game/UpdateMinigameLoaderData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Game
+{
+    public class UpdateMinigameLoaderData : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _languageText;
+
+        [SerializeField]
+        private Slider _slider;
+
+        [SerializeField]
+        private Text _descriptionText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
