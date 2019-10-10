7802055d8d4d6324d283e9e2034fade282d085c9
Paweł Idzikowski <iddziku@gmail.com>
2019-10-10 19:31:45 +0200

    Dodanie skryptu TextStorage w którym będą przechowywane teksty na ekrany wczytywania

---
+++ projectZero/Assets/Scripts/Game/TextStorage.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    // This file stores all texts that can be shown 
+    // during minigame loading
+
+    public class TextStorage : MonoBehaviour
+    {
+        public static List<string> Texts { get; set; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Texts = new List<string>();
+
+            if (LevelLoader.CrossSceneInformation.Contains("C#"))
+            {
+                Texts = AddCsharpDataToTexts();
+                Debug.Log("Successfully added C# data to Texts");
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("Java"))
+            {
+                Texts = AddJavaDataToTexts();
+                Debug.Log("Successfully added Java data to Texts");
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("JS"))
+            {
+                Texts = AddJSDataToTexts();
+                Debug.Log("Successfully added JavaScript data to Texts");
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("PHP"))
+            {
+                Texts = AddPhpDataToTexts();
+                Debug.Log("Successfully added PHP data to Texts");
+            }
+            else if (LevelLoader.CrossSceneInformation.Contains("HTML"))
+            {
+                Texts = AddHtmlDataToTexts();
+                Debug.Log("Successfully added HTML data to Texts");
+            }
+            else
+            {
+                Debug.LogWarning("Nothing was added to Texts!");
+            }
+        }
+
+        List<string> AddCsharpDataToTexts()
+        {
+           return new List<string>
+           {
+               "Jezeli cos to cos",
+               "Jak to to to"
+           };
+        }
+
+        List<string> AddJavaDataToTexts()
+        {
+            return new List<string>
+            {
+                "Jezeli cos to cos",
+                "Jak to to to"
+            };
+        }
+
+        List<string> AddJSDataToTexts()
+        {
+            return new List<string>
+            {
+                "Jezeli cos to cos",
+                "Jak to to to"
+            };
+        }
+
+        List<string> AddPhpDataToTexts()
+        {
+            return new List<string>
+            {
+                "Jezeli cos to cos",
+                "Jak to to to"
+            };
+        }
+
+        List<string> AddHtmlDataToTexts()
+        {
+            return new List<string>
+            {
+                "Jezeli cos to cos",
+                "Jak to to to"
+            };
+        }
+    }
+}
