3b12ead3647147f8e585c69a1cbfa147baa45f01
Paweł Idzikowski <iddziku@gmail.com>
2019-10-13 18:04:19 +0200

    Dodanie managera sklepu - gdy bedzie postac w obszarze wyznaczonym to moze wejsc do sklepu + design

---
+++ projectZero/Assets/Scripts/Game/Shop/ShopManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game.Shop
+{
+    public class ShopManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _tip;
+
+        [SerializeField]
+        private GameObject _shopUI;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                _tip.SetActive(true);
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    _shopUI.SetActive(true);
+                }
+                else if (Input.GetKeyDown(KeyCode.N))
+                {
+                    _shopUI.SetActive(false);
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                _tip.SetActive(false);
+
+                _shopUI.SetActive(false);
+            }
+        }
+    }
+}
