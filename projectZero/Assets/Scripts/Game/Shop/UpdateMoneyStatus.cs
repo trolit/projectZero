293e010ffa821ee169e2379332dbd1dd82cc7514
Paweł Idzikowski <iddziku@gmail.com>
2019-10-13 18:03:58 +0200

    Dodanie skryptu aktualizujacego liczbe pieniedzy na ekranie sklepu

---
+++ projectZero/Assets/Scripts/Game/Shop/UpdateMoneyStatus.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Game.Shop
+{
+    public class UpdateMoneyStatus : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _currentMoneyText;
+
+        private void Update()
+        {
+            var money = PlayerPrefs.GetInt("money");
+
+            _currentMoneyText.text = money.ToString();
+        }
+    }
+}
