8e6afe5c2d4a1ad26c658696ab88abd5f511a785
gradampl <gr_adam@o2.pl>
2019-10-14 11:30:25 +0200

    Dodanie skryptu SkyscraperPrizeManager

---
+++ projectZero/Assets/Scripts/Minigame_Skyscraper/SkyscraperPrizeManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Minigame_Skyscraper
+{
+    public class SkyscraperPrizeManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _prizeText;
+
+        public static int MistakesCount = 0;
+
+        private int _prize;
+
+        public void TransferMoney()
+        {
+            if (_prize < 0)
+            {
+                _prize = 0;
+            }
+
+            var currentMoney = PlayerPrefs.GetInt("money");
+
+            var newMoney = currentMoney + _prize;
+
+            PlayerPrefs.SetInt("money", newMoney);
+        }
+
+        private int CountSkyscraperPrize()
+        {
+            return 250 - (MistakesCount * 15);
+        }
+
+        private void Awake()
+        {
+            _prize = CountSkyscraperPrize();
+
+            _prizeText.text = _prize.ToString();
+        }
+    }
+}
