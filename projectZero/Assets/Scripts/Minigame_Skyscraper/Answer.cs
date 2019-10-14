721cb672283dc89510b6240ddcac21bab81bf1f7
gradampl <gr_adam@o2.pl>
2019-10-14 10:59:55 +0200

    Dodanie mechaniki wieżowca

---
+++ projectZero/Assets/Scripts/Minigame_Skyscraper/Answer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Minigame_Skyscraper
+{
+    public class Answer : MonoBehaviour
+    {
+        [SerializeField]
+        protected AudioSource Source;
+
+        [SerializeField]
+        protected AudioClip Clip;
+
+        protected bool IsOnQuestion = false;
+
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                IsOnQuestion = true;
+            }
+        }
+
+        void OnCollisionStay(Collision col)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                IsOnQuestion = true;
+            }
+        }
+
+        void OnCollisionExit(Collision col)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                IsOnQuestion = false;
+            }
+        }
+    }
+}
---
+++ projectZero/Assets/Scripts/Minigame_Skyscraper/WrongAnswer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Minigame_Skyscraper
+{
+    public class WrongAnswer : Answer
+    {
+        [SerializeField]
+        private GameObject _wrongAnswerObject;
+
+        private void Start()
+        {
+            _wrongAnswerObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (IsOnQuestion == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    _wrongAnswerObject.SetActive(true);
+
+                    Source.PlayOneShot(Clip);
+                }
+            }
+        }
+
+        
+    }
+}
