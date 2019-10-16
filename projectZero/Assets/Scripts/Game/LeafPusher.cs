5845b5d57c0bfd33a17f34889868cea7a77419a5
Paweł Idzikowski <iddziku@gmail.com>
2019-10-16 23:55:26 +0200

    Modyfikacja LeafPusher dodanie materialu LeafMaterial

--- projectZero/Assets/Scripts/Game/LeafPusher.cs
+++ projectZero/Assets/Scripts/Game/LeafPusher.cs
@@ -13,19 +13,19 @@ namespace Assets.Scripts.Game
 
         [SerializeField]
         [Range(0.0f, 50.0f)]
-        private int _upPower = 10;
+        private int _upPower = 27;
 
         [SerializeField]
         [Range(0.0f, 75.0f)]
-        private int _downPower = 10;
+        private int _downPower = 63;
 
         [SerializeField]
         [Range(0.0f, 50.0f)]
-        private float _sidePower = 25f;
+        private float _sidePower = 19.9f;
 
         [SerializeField]
         [Range(0.0f, 50.0f)]
-        private float _rotationPower = 25f;
+        private float _rotationPower = 30.4f;
 
         private void Start()
         {
@@ -37,18 +37,28 @@ namespace Assets.Scripts.Game
             if (other.gameObject.tag == "Player")
             {
                 Vector3 dir = other.transform.position - transform.position;
-                dir.Normalize();
 
+                dir.Normalize();
 
                 _rigidbody.AddForce(-dir * _sidePower, ForceMode.Impulse);
                 _rigidbody.AddForce(transform.up * _upPower, ForceMode.VelocityChange);
-                _rigidbody.AddTorque(dir * _rotationPower, ForceMode.VelocityChange);
+
+                var torqueRandomizer = Random.Range(0, 2);
+
+                if (torqueRandomizer == 1)
+                {
+                    _rigidbody.AddTorque(-dir * _rotationPower, ForceMode.VelocityChange);
+                }
+                else
+                {
+                    _rigidbody.AddTorque(dir * _rotationPower, ForceMode.VelocityChange);
+                }
             }
         }
 
-        void FixedUpdate()
+        private void FixedUpdate()
         {
-            _rigidbody.AddForce(new Vector3(0, -2, 0) * 1f, ForceMode.VelocityChange);
+            _rigidbody.AddForce(new Vector3(0, -2, 0) * 1.1f, ForceMode.VelocityChange);
         }
     }
 }
