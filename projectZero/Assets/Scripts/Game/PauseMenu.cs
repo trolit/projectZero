78823bbe1c6136f372106bf599011031ec8b72d9
Paweł Idzikowski <iddziku@gmail.com>
2019-10-18 23:08:59 +0200

    Rozbudowa ekranu escape (dodanie mozliwosci manipulacji dzwiekiem)

--- projectZero/Assets/Scripts/Game/PauseMenu.cs
+++ projectZero/Assets/Scripts/Game/PauseMenu.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Game
 {
@@ -9,14 +10,24 @@ namespace Assets.Scripts.Game
 
         public GameObject PauseMenuUi;
 
+        public GameObject AudioMenu;
+
+        public Text AudioText;
+
         // Update is called once per frame
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                if (GameIsPaused)
+                if (GameIsPaused && AudioMenu.activeInHierarchy == false)
                 {
                     Resume();
+                }
+                else if (AudioMenu.activeInHierarchy)
+                {
+                    AudioText.text = "Aby wrócić do poprzedniego ekranu wciśnij przycisk powrotu.";
+
+                    Invoke("TextClearer", 2f);
                 }
                 else
                 {
@@ -41,13 +52,18 @@ namespace Assets.Scripts.Game
             SceneManager.LoadSceneAsync(scene.name);
         }
 
-        void Pause()
+        private void Pause()
         {
             PauseMenuUi.SetActive(true);
 
             Time.timeScale = 0f;
 
             GameIsPaused = true;
+        }
+
+        private void TextClearer()
+        {
+            AudioText.text = "";
         }
     }
 }
