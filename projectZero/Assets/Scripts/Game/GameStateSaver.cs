91561463ba23737cfc4cf5648743e2b7e22cc34a
Paweł Idzikowski <iddziku@gmail.com>
2019-10-21 21:15:27 +0200

    Dodanie skryptu GameStateSaver

---
+++ projectZero/Assets/Scripts/Game/GameStateSaver.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class GameStateSaver : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _playerTransform;
+
+        public void SaveGameState()
+        {
+            var i = 0;
+
+            var modelsAmount = _playerTransform.childCount;
+
+            while (i < modelsAmount)
+            {
+                Debug.Log("Pracuje -> " + i);
+
+                if (_playerTransform.GetChild(i).gameObject.activeInHierarchy)
+                {
+                    PlayerPrefs.SetFloat("playerX", _playerTransform.GetChild(i).position.x);
+
+                    PlayerPrefs.SetFloat("playerZ", _playerTransform.GetChild(i).position.z);
+
+                    Debug.Log("Player position saved successfully!");
+
+                    break;
+                }
+
+
+                i++;
+
+                if (i >= _playerTransform.childCount)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
