2fa57600e768bba2ee3801f9f0527096fcfe12a7
gradampl <gr_adam@o2.pl>
2019-10-30 11:11:38 +0100

    Dodanie modelu myszy, muchomora i królika

---
+++ projectZero/Assets/Models/NPCs/mushroom/Script/MushroomMon_Ani_Test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomMon_Ani_Test : MonoBehaviour {
+
+	public const string IDLE	= "Idle";
+	public const string RUN		= "Run";
+	public const string ATTACK	= "Attack";
+	public const string DAMAGE	= "Damage";
+	public const string DEATH	= "Death";
+
+	Animation anim;
+
+	void Start () {
+		anim = GetComponent<Animation>();
+	}
+
+	public void IdleAni (){
+		anim.CrossFade (IDLE);
+	}
+
+	public void RunAni (){
+		anim.CrossFade (RUN);
+	}
+
+	public void AttackAni (){
+		anim.CrossFade (ATTACK);
+	}
+
+	public void DamageAni (){
+		anim.CrossFade (DAMAGE);
+	}
+
+	public void DeathAni (){
+		anim.CrossFade (DEATH);
+	}
+}
