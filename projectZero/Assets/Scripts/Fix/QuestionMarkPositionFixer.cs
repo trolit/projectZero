17d0508ae4aa56277f25b791dd0afd3c2c3309b7
Paweł Idzikowski <iddziku@gmail.com>
2019-11-01 11:08:01 +0100

    Praca nad znakiem zapytania

---
+++ projectZero/Assets/Scripts/Fix/QuestionMarkPositionFixer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Fix
+{
+    public class QuestionMarkPositionFixer : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _parentTransform;
+
+        private void LateUpdate()
+        {
+            gameObject.transform.parent = _parentTransform;
+        }
+    }
+}
