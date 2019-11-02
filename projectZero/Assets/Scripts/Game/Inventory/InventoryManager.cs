2a8689903474b97e50b904b427653f7d4a646de0
gradampl <gr_adam@o2.pl>
2019-11-02 14:18:06 +0100

    Poprawiony InventoryManager

--- projectZero/Assets/Scripts/Game/Inventory/InventoryManager.cs
+++ projectZero/Assets/Scripts/Game/Inventory/InventoryManager.cs
@@ -7,33 +7,13 @@ namespace Assets.Scripts.Game.Inventory
         [SerializeField]
         private GameObject _inventoryUi;
 
-        //private bool isActive = false;
-
-        //// Start is called before the first frame update
-        //void Start()
-        //{
-
-        //}
-
         // Update is called once per frame
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.B))
             {
                 _inventoryUi.SetActive(!_inventoryUi.activeInHierarchy);
-                //isActive = true;
             }
-
-            //if (isActive)
-            //{
-            //    if (Input.GetKeyDown(KeyCode.B))
-            //    {
-            //        _backpackUi.SetActive(false);
-            //        isActive = false;
-            //    }
-
-            //}
-
         }
     }
 
