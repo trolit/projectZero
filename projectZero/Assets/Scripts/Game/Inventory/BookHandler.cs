90c3f40289c2338b27a87ecbd6ccd5139f2af877
gradampl <gr_adam@o2.pl>
2019-11-02 15:24:33 +0100

    Przycisk "Przeczytaj" już działa dla 2 książek z C#

---
+++ projectZero/Assets/Scripts/Game/Inventory/BookHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Game.Inventory
+{
+    public class BookHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private string productKey;
+
+        [SerializeField]
+        private Button _button;
+
+        // Update is called once per frame
+        void Update()
+        {
+            var bookStatus = PlayerPrefs.GetInt(productKey);
+           
+            if (bookStatus != 1)
+            {
+                // przycisk interactable na false
+                _button.interactable = false;
+            }
+            else
+            {
+                _button.interactable = true;
+            }
+        }
+    }
+}
+    
+
+
+
