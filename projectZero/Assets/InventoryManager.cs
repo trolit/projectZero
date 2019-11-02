9425ec0c822dfc5bdf43689adabf90ce175d9c41
gradampl <gr_adam@o2.pl>
2019-11-02 13:28:25 +0100

    Inventory_Canvas pojawia się (i znika) po naciśnięciu B

---
+++ projectZero/Assets/InventoryManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Camera
+{
+    public class InventoryManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _inventoryUi;
+
+        //private bool isActive = false;
+
+        //// Start is called before the first frame update
+        //void Start()
+        //{
+
+        //}
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                _inventoryUi.SetActive(!_inventoryUi.activeInHierarchy);
+                //isActive = true;
+            }
+
+            //if (isActive)
+            //{
+            //    if (Input.GetKeyDown(KeyCode.B))
+            //    {
+            //        _backpackUi.SetActive(false);
+            //        isActive = false;
+            //    }
+
+            //}
+
+        }
+    }
+
+}
