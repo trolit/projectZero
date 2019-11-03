1b554267a2909789893aa243af799f7246b0dd7f
gradampl <gr_adam@o2.pl>
2019-11-03 18:36:19 +0100

    Skrypt do kończenia czytania książki (w Inventory)

---
+++ projectZero/Assets/Scripts/Game/Inventory/PageCurlManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game.Inventory
+{
+    public class PageCurlManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _pageCurlUi;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_pageCurlUi.activeInHierarchy)
+            {
+                if (Input.GetKeyDown(KeyCode.K))
+                {
+                    _pageCurlUi.SetActive(false);
+                }
+            }
+            
+        }
+    }
+
+}
