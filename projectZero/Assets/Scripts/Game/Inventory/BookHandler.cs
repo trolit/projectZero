317860fe62833a66605f7dc7f7e7f5c7114400f6
gradampl <gr_adam@o2.pl>
2019-11-06 10:24:21 +0100

    Modyfikacja skryptu BookHandler

--- projectZero/Assets/Scripts/Game/Inventory/BookHandler.cs
+++ projectZero/Assets/Scripts/Game/Inventory/BookHandler.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.Game.Inventory
@@ -14,18 +11,50 @@ namespace Assets.Scripts.Game.Inventory
         [SerializeField]
         private Button _button;
 
+        [SerializeField]
+        private GameObject _bookObject;
+
+        private bool _isInteractable = false;
+
         // Update is called once per frame
         private void Update()
         {
+            if (_isInteractable == false)
+            {
+                SetButtonInteractibility();
+            }
+
+            CloseBookOnKey();
+        }
+
+        public void OpenBook()
+        {
+            _bookObject.SetActive(true);
+        }
+
+        private void CloseBookOnKey()
+        {
+            if (gameObject.activeInHierarchy && Input.GetKeyDown(KeyCode.K))
+            {
+                _bookObject.SetActive(false);
+            }
+        }
+
+        private void SetButtonInteractibility()
+        {
             var bookStatus = PlayerPrefs.GetInt(_bookKey);
-           
+
             if (bookStatus != 1) // if the book has not been bought yet
             {
                 _button.interactable = false;
+
+                _isInteractable = false;
             }
             else
             {
                 _button.interactable = true;
+
+                _isInteractable = true;
             }
         }
     }
