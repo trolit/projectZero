016ca3d544e69e82cffea2eaa82d2a0c452dd98d
Paweł Idzikowski <iddziku@gmail.com>
2019-11-07 17:15:18 +0100

    Modyfikacja GameStateClearer

--- projectZero/Assets/Scripts/Game/GameStateClearer.cs
+++ projectZero/Assets/Scripts/Game/GameStateClearer.cs
@@ -30,7 +30,11 @@ namespace Assets.Scripts.Game
                 "JS01",
                 "JS02",
                 "JS03",
-                "JS04"
+                "JS04",
+                "PHP01",
+                "PHP02",
+                "PHP03",
+                "PHP04"
             };
 
             string[] levelKeys =
@@ -108,6 +112,102 @@ namespace Assets.Scripts.Game
             PlayerPrefs.Save();
 
             Debug.Log("Game status cleared!");
+        }
+
+        public void ClearSpecificDataOnNewGame()
+        {
+            #region Keys data
+
+            string[] bookKeys =
+{
+                "C#01",
+                "C#02",
+                "C#03",
+                "C#04",
+                "HTML01",
+                "HTML02",
+                "HTML03",
+                "HTML04",
+                "J01",
+                "J02",
+                "J03",
+                "J04",
+                "JS01",
+                "JS02",
+                "JS03",
+                "JS04",
+                "PHP01",
+                "PHP02",
+                "PHP03",
+                "PHP04"
+            };
+
+            string[] levelKeys =
+            {
+                "PU_C#1",
+                "PU_C#2",
+                "PU_HTML#1",
+                "PU_HTML#2",
+                "PU_Java#1",
+                "PU_Java#2",
+                "PU_JS#1",
+                "PU_JS#2",
+                "PU_PHP#1",
+                "PU_PHP#2",
+                "PI_C#1",
+                "PI_C#2",
+                "PI_HTML#1",
+                "PI_HTML#2",
+                "PI_Java#1",
+                "PI_Java#2",
+                "PI_JS#1",
+                "PI_JS#2",
+                "PI_PHP#1",
+                "PI_PHP#2",
+                "MA_C#1",
+                "MA_C#2",
+                "MA_HTML#1",
+                "MA_HTML#2",
+                "MA_Java#1",
+                "MA_Java#2",
+                "MA_JS#1",
+                "MA_JS#2",
+                "MA_PHP#1",
+                "MA_PHP#2",
+                "SK_C#1",
+                "SK_C#2",
+                "SK_HTML#1",
+                "SK_HTML#2",
+                "SK_Java#1",
+                "SK_Java#2",
+                "SK_JS#1",
+                "SK_JS#2",
+                "SK_PHP#1",
+                "SK_PHP#2"
+            };
+
+            string[] otherKeys =
+            {
+                "money",
+                "model",
+                "csharp",
+                "html",
+                "php",
+                "java",
+                "javascript"
+            };
+
+            #endregion
+
+            ClearIntTypedKeys(bookKeys);
+
+            ClearIntTypedKeys(levelKeys);
+
+            ClearIntTypedKeys(otherKeys);
+
+            ClearUniquePassedKeys(levelKeys);
+
+            PlayerPrefs.Save();
         }
 
         private void ClearIntTypedKeys(string[] array)
