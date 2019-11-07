158ae4641cfce52954f41178b60d4cb51b29e30e
Paweł Idzikowski <iddziku@gmail.com>
2019-11-07 16:15:47 +0100

    Utworzenie skryptu NPCCOllector skupiającego wszystkie NPC

---
+++ projectZero/Assets/Scripts/Game/NPCCollector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class NPCCollector : MonoBehaviour
+    {
+        public static NPCCollector instance = null;
+
+        public List<GameObject> NpcGameObjects;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+
+                Debug.LogWarning($"Since there is already one instance of NPCCollector," +
+                                 $" instance on {gameObject.name} was destroyed.");
+            }
+
+            if (NpcGameObjects == null)
+            {
+                Debug.LogError($"No non-playable characters assigned on {gameObject.name}");
+                Debug.Break();
+            }
+        }
+    }
+}
