109c809dbadeed8efaa94780763d7268503a362f
Paweł Idzikowski <iddziku@gmail.com>
2019-11-19 18:19:19 +0100

    Dodanie sceny Processing i skryptu ProcessingHandler

---
+++ projectZero/Assets/Scripts/Game/ProcessingHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Game
+{
+    public class ProcessingHandler : MonoBehaviour
+    {
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(3.5f);
+
+            LoadNextSceneAsync();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                LoadNextSceneAsync();
+            }
+        }
+
+        private void LoadNextSceneAsync()
+        {
+            SceneManager.LoadSceneAsync("MenuLoader");
+        }
+    }
+}
