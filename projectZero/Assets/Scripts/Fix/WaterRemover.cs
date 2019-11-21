48eaa38c8111d70667f79f1dd80079dabc83c1ed
Paweł Idzikowski <iddziku@gmail.com>
2019-11-21 15:04:09 +0100

    Dodanie skryptu WaterRemover

---
+++ projectZero/Assets/Scripts/Fix/WaterRemover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Fix
+{
+    public class WaterRemover : MonoBehaviour
+    {
+        private void Awake()
+        {
+            int qualityIndex = PlayerPrefs.GetInt("quality");
+
+            // 0 => Very low quality
+            // 1 => Low quality
+            if (qualityIndex == 0 || qualityIndex == 1)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
