2aed7b13f97e79bc162a4f250c5e6f6e0bc3f115
Paweł Idzikowski <iddziku@gmail.com>
2019-12-05 21:27:36 +0100

    Dodanie skryptu ExtraCharacterToggler

---
+++ projectZero/Assets/Scripts/CharacterCreation/ExtraCharacterToggler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CharacterCreation
+{
+    public class ExtraCharacterToggler : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _knightButton;
+
+        private void Start()
+        {
+            var medalsAmount = PlayerPrefs.GetInt("medalsUnlocked");
+
+            if (medalsAmount >= 2)
+            {
+                _knightButton.SetActive(true);
+
+                gameObject.transform.localPosition = new Vector3(2f, gameObject.transform.localPosition.y, 0f);
+            }
+            else
+            {
+                _knightButton.SetActive(false);
+
+                gameObject.transform.localPosition = new Vector3(64.7f, gameObject.transform.localPosition.y, 0f);
+            }
+        }
+    }
+}
