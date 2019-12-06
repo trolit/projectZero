93a0576c9a21d0d8010b954a411a808708f7a5ba
Paweł Idzikowski <iddziku@gmail.com>
2019-12-06 23:05:26 +0100

    Utworzenie skryptów PadChecker oraz PuzzleManager

---
+++ projectZero/Assets/Scripts/Minigame_Puzzle/PuzzleManager.cs
@@ -0,0 +1,43 @@
+﻿using Assets.Scripts.Game;
+using UnityEngine;
+
+namespace Assets.Scripts.Minigame_Puzzle
+{
+    public class PuzzleManager : MonoBehaviour
+    {
+        public static int PuzzleStatus;
+
+        [Tooltip("Define how many puzzles are set in level. Value must be greater than 0.")]
+        [SerializeField]
+        private int _puzzleAmount;
+
+        private void Start()
+        {
+            if (_puzzleAmount <= 0)
+            {
+                Debug.LogError("_puzzleAmount value cannot be less/equal than 0");
+                Debug.Break();
+            }
+
+            PuzzleStatus = 0;
+        }
+
+        private void Update()
+        {
+            if (PuzzleStatus >= _puzzleAmount)
+            {
+                TriggerWin();
+            }
+        }
+
+        private void TriggerWin()
+        {
+            var script = GetComponent<WinScript>();
+
+            script.enabled = true;
+
+            // Turn off Update()
+            enabled = false;
+        }
+    }
+}
